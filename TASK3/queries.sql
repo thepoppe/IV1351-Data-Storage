19935825bc5cada0cbf3491f472b26dc1d17be2e
thepoppe <pontus.morin@hotmail.se>
2023-12-01 16:50:25 +0100

    Updated table looks and alt query3 added

--- TASK3/queries.sql
+++ TASK3/queries.sql
@@ -1,19 +1,19 @@
 --query 1 display all lessons
 SELECT
-    TO_CHAR(timeslot.slot_date, 'Month') AS MONTH,
-    COUNT(*) AS total,
+    TO_CHAR(timeslot.slot_date, 'Mon') AS "Month",
+    COUNT(*) AS "Total Lessons",
     COUNT(*) FILTER (
         WHERE
             info.lesson_type = 'Individual'
-    ) AS Individual,
+    ) AS "Individual",
     COUNT(*) FILTER (
         WHERE
             info.lesson_type = 'Group'
-    ) AS GROUP,
+    ) AS "Group",
     COUNT(*) FILTER (
         WHERE
             info.lesson_type = 'Ensemble'
-    ) AS Ensemble
+    ) AS "Ensemble"
 FROM
     lesson
     INNER JOIN price_info AS info ON lesson.price_info_id = info.price_info_id
@@ -26,15 +26,15 @@ WHERE
             timeslot.slot_date
     ) = 2023
 GROUP BY
-    TO_CHAR(timeslot.slot_date, 'Month')
+    TO_CHAR(timeslot.slot_date, 'Mon')
 ORDER BY
-    TO_CHAR(timeslot.slot_date, 'Month');
+    TO_CHAR(timeslot.slot_date, 'Mon');
 
 
 --query 2
 SELECT
-    number_of_siblings,
-    COUNT(person_id) AS number_of_students
+    number_of_siblings AS "No of Siblings",
+    COUNT(person_id) AS "No of Students"
 FROM
     (
         SELECT
@@ -84,14 +84,46 @@ GROUP BY
     person.last_name;
 
 
+--alt use the boolean is_available => one less join
+SELECT
+    instr_time.person_id AS "Instructor ID",
+    person.first_name AS "First Name",
+    person.last_name AS "Last Name",
+    COUNT(instr_time.person_id) AS "No of Lessons"
+FROM
+    instructor_timeslot AS instr_time
+    LEFT JOIN instructor ON instructor.person_id = instr_time.person_id
+    LEFT JOIN person ON person.person_id = instructor.person_id
+    LEFT JOIN timeslot AS TIME ON TIME.slot_id = instr_time.slot_id
+WHERE
+    instr_time.is_available = FALSE
+    AND EXTRACT(
+        MONTH
+        FROM
+            TIME.slot_date
+    ) = EXTRACT(
+        MONTH
+        FROM
+            CURRENT_DATE
+    )
+GROUP BY
+    instr_time.person_id,
+    person.first_name,
+    person.last_name;
+
+
 --query 4
 SELECT
     TO_CHAR(subquery.slot_date, 'Dy') AS DAY,
     ensemble.target_genre AS Genre,
     subquery.slot_date,
     CASE
-        WHEN (ensemble.maximum_students - COUNT(stu_les.person_id)) < 1 THEN 'No Seats'
-        WHEN (ensemble.maximum_students - COUNT(stu_les.person_id)) > 2 THEN 'Many Seats'
+        WHEN (
+            ensemble.maximum_students - COUNT(stu_les.person_id)
+        ) < 1 THEN 'No Seats'
+        WHEN (
+            ensemble.maximum_students - COUNT(stu_les.person_id)
+        ) > 2 THEN 'Many Seats'
         ELSE '1 or 2 Seats'
     END AS "No of Free Seats"
 FROM
